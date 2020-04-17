14e021b6866fc91a5ec3a4ff2dfd09ef3707f893
benjacoblee <benjacoblee@gmail.com>
2020-04-17 17:30:13 +0800

    can update article

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Basically a blog.
 ## Technologies 
 
 ## General Approach
+Started by building back-end. Configure API endpoints for basic CRUD operations.
 
 ## Installation Instructions
 
--- routes/api/articles.js
+++ routes/api/articles.js
@@ -23,12 +23,18 @@ router.post(
     const { title, body, tags } = req.body;
     const { userID } = req.user;
 
+    let tagsArr = [];
+
+    if (tags) {
+      tagsArr = tags.split(",").map((tag) => {
+        return tag.trim();
+      });
+    }
+
     let article = new Article({
       title,
       body,
-      tags: tags.split(",").map((tag) => {
-        return tag.trim();
-      }),
+      tags: tagsArr,
       userID
     });
 
@@ -43,6 +49,60 @@ router.post(
   }
 );
 
-router.put("/:id", auth, (req, res) => {});
+router.patch(
+  "/:id",
+  auth,
+  [
+    check("title", "Title is required!").not().isEmpty(),
+    check("body", "Body is required!").not().isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+
+    let article = await Article.findById(req.params.id);
+
+    if (req.user.userID !== article.userID) {
+      return res.status(400).json({
+        msg: "Must be author of article to edit!"
+      });
+    }
+
+    const { title, body, tags } = req.body;
+    let tagsArr = [];
+
+    if (tags) {
+      tagsArr = tags.split(",").map((tag) => {
+        return tag.trim();
+      });
+    }
+
+    try {
+      article = await Article.findOneAndUpdate(
+        {
+          _id: req.params.id
+        },
+        {
+          title,
+          body,
+          tags: tagsArr,
+          dateEdited: Date.now()
+        },
+        { new: true }
+      );
+
+      res.json(article);
+    } catch (err) {
+      res.status(500).json({
+        msg: err.message
+      });
+    }
+  }
+);
 
 module.exports = router;
