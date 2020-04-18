b948b25e97e567fd4965707b8f573dc047901300
benjacoblee <benjacoblee@gmail.com>
2020-04-18 17:14:53 +0800

    initial setup for comments route

---
+++ routes/api/comments.js
@@ -0,0 +1,6 @@
+// find article, add comments to array
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,8 +9,9 @@ connectDB();
 app.use(express.json());
 
 app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"))
-app.use("/api/articles", require("./routes/api/articles"))
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/articles", require("./routes/api/articles"));
+app.use("/api/articles/:id/comments", require("./routes/api/comments"));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
