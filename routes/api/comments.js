4f09d350d8e6d8ff4a2e8364ee767cd81729e6db
benjacoblee <benjacoblee@gmail.com>
2020-04-19 14:04:09 +0800

    bring in required files and modules, add comments

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -2,5 +2,24 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const Article = require("../../models/Article");
+const Comment = require("../../models/Comment");
+
+router.get("/", (req, res) => {
+  // get all comments for this article
+});
+
+router.post("/", (req, res) => {
+  // post new comment
+});
+
+router.patch("/:id", (req, res) => {
+  // edit comment
+});
+
+router.delete("/:id", (req, res) => {
+  // delete comment
+});
 
 module.exports = router;
