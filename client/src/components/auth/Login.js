43d1e9eae54c6488f7ae5962f8608725c34559ad
benjacoblee <benjacoblee@gmail.com>
2020-04-23 15:05:30 +0800

    set up state and functions to update and submit form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,24 +1,65 @@
-import React, { Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [formData, updateFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <Fragment>
-      <Form className="login-form">
+      <Form
+        className="login-form"
+        onSubmit={(e) => {
+          onSubmit(e);
+        }}
+      >
         <h1 className="text-center mb-2">Login</h1>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              onChange(e);
+            }}
+            required
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => {
+              onChange(e);
+            }}
+            required
+          />
         </Form.Group>
         <Form.Group>
           <Form.Text className="text-muted">
-            Don't have an account? <Link to="/register">Sign up</Link>
+            Don't have an account? <Link to="/register">Sign up!</Link>
           </Form.Text>
         </Form.Group>
         <Button variant="primary" type="submit">
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,25 +1,71 @@
-import React, { Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import { Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [formData, updateFormData] = useState({
+    email: "",
+    password: "",
+    password2: ""
+  });
+
+  const { email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    updateFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password === password2) {
+      return console.log("WE NEED TO SUBMIT");
+    }
+  };
+
   return (
     <Fragment>
-      <Form className="login-form">
+      <Form className="login-form" onSubmit={(e) => onSubmit(e)}>
         <h1 className="text-center mb-2">Register</h1>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              onChange(e);
+            }}
+            required
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => {
+              onChange(e);
+            }}
+            required
+          />
         </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
+        <Form.Group controlId="formBasicPassword2">
           <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password2"
+            value={password2}
+            onChange={(e) => {
+              onChange(e);
+            }}
+            required
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
