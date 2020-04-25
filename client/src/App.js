f2b14af55a96bc54fa28b7bfe509210524e1ce11
upieez <samuelhww@gmail.com>
2020-04-25 20:19:18 +0800

    style: remove Switch
    Not used and but declared

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/layout/Header";
 import Login from "./components/auth/Login";
