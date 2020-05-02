1edfd7f613610fcf0d9397bc4dd940d6c21ca564
benjacoblee <benjacoblee@gmail.com>
2020-05-02 15:54:49 +0800

    some styling

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -1,7 +1,9 @@
 import React, { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
+import { Card, Button, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import * as actions from "../actions";
+import Moment from "react-moment";
 
 const Articles = (props) => {
   useEffect(() => {
@@ -13,15 +15,31 @@ const Articles = (props) => {
     if (props.articles.length > 0) {
       return props.articles.map((article) => {
         return (
-          <div key={article._id}>
-            <Link to={`/articles/${article._id}`}>
-              <p>{article.title}</p>
-            </Link>
-          </div>
+          <Card className="my-3">
+            <Card.Body>
+              <Card.Title>{article.title} </Card.Title>
+              <Card.Subtitle className="mb-2">
+                <small>
+                  {article.postedBy.name}{" "}
+                  <span className="text-muted">
+                    <Moment format="DD MMM YYYY">{article.dateCreated}</Moment>
+                  </span>
+                </small>
+              </Card.Subtitle>
+              <Card.Text>{article.body}</Card.Text>
+              <Button as={Link} to={`/articles/${article._id}`}>
+                Read More
+              </Button>
+            </Card.Body>
+          </Card>
         );
       });
     }
-    return <p>Spinner</p>;
+    return (
+      <Spinner className="mt-3 mx-auto" animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
   };
   return <Fragment>{renderArticles()}</Fragment>;
 };
