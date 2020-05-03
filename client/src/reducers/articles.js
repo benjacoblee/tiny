a3d37d30ad97312653d1a5e7898890bc7c39de11
benjacoblee <benjacoblee@gmail.com>
2020-05-03 18:24:02 +0800

    infinite scroll seems to be working

--- client/src/reducers/articles.js
+++ client/src/reducers/articles.js
@@ -3,7 +3,7 @@ import { FETCH_ARTICLES } from "../actions/types";
 export default (state = [], action) => {
   switch (action.type) {
     case FETCH_ARTICLES:
-      return [...action.payload];
+      return [...state, ...action.payload];
     default:
       return state;
   }
