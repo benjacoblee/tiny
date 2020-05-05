25e54c4b33ad2c7147e47d2c9f6ddc26c2bf52e2
benjacoblee <benjacoblee@gmail.com>
2020-05-06 06:06:42 +0800

    styling for dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import * as actions from "../actions";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
 const Dashboard = ({ dashboard, fetchDashboardDetails, history }) => {
   const token = sessionStorage.getItem("jwtToken");
@@ -18,13 +19,15 @@ const Dashboard = ({ dashboard, fetchDashboardDetails, history }) => {
     if (articles.length > 0) {
       return articles.map((article) => {
         return (
-          <Link
-            key={article._id}
-            className="text-center"
-            to={`/articles/${article._id}`}
-          >
-            {article.title}
-          </Link>
+          <ListGroup.Item key={article._id}>
+            <Link
+              key={article._id}
+              className="text-center"
+              to={`/articles/${article._id}`}
+            >
+              {article.title}
+            </Link>
+          </ListGroup.Item>
         );
       });
     }
@@ -40,10 +43,10 @@ const Dashboard = ({ dashboard, fetchDashboardDetails, history }) => {
           <div className="col-6">
             {user.name ? <p>{user.name}</p> : null}
             {user.bio ? <p>{user.bio}</p> : null}
+            <Link className="mx-auto" to="/profile/edit">
+              Edit Profile Details
+            </Link>
           </div>
-          <Link className="mx-auto" to="/profile/edit">
-            Edit Profile Details
-          </Link>
         </div>
       );
     }
@@ -51,17 +54,35 @@ const Dashboard = ({ dashboard, fetchDashboardDetails, history }) => {
 
   return (
     <div style={{ marginTop: "20px" }} className="row">
-      <div className="col-5">
-        <h6 className="text-center">My Articles</h6>
-        {articles ? renderArticles() : null}
+      <div className="col-6">
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <h6 className="text-center">My Articles</h6>
+            </Card.Title>
+            <ListGroup variant="flush">
+              {articles ? renderArticles() : null}
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <Card className="mt-1">
+          <Card.Body>
+            <Card.Title>
+              <h6 className="text-center">Number of Comments</h6>
+            </Card.Title>
+            <Card.Text className="text-center">
+              {dashboard.numberOfComments}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
-      <div className="col-2">
-        <h6 className="text-center">Comments</h6>
-        <p className="text-center">{numberOfComments}</p>
-      </div>
-      <div className="col-5">
-        <h6 className="text-center">Profile</h6>
-        {renderProfile()}
+      <div className="col-6">
+        <Card>
+          <Card.Body>
+            <h6 className="text-center">Profile</h6>
+            {renderProfile()}
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
